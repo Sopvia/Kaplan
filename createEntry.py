b60631e8bc3aa35c4b9af54c9d0e15edc9356575
Sopvia <112272251+Sopvia@users.noreply.github.com>
2024-07-20 16:58:57 +0200

    translation works, added icons, button to delete entry

--- createEntry.py
+++ createEntry.py
@@ -7,9 +7,21 @@ from datetime import datetime
 connection = sqlite3.connect("addressBook.db")
 cursor = connection.cursor()
 
+getLang = """SELECT language FROM settings;"""
+cursor.execute(getLang)
+language = cursor.fetchone()
+
+if language == None:
+    cursor.execute("INSERT INTO settings (language) VALUES (?)", ("english",))
+    connection.commit()
+
+if language[0] == "german":
+    var = 1
+else:
+    var = 0
+
 
 def open_createEntry():
-    
     createEntry = ctk.CTkToplevel(fg_color="black")
     createEntry.title("New Entry")
     createEntry.geometry("600x600")
@@ -24,19 +36,19 @@ def open_createEntry():
     createEntry.grid_columnconfigure(2, weight=2)
 
 
-    createEntry.category = ctk.CTkLabel(createEntry, text=categoryText, height=12, width=12, text_color="white")
+    createEntry.category = ctk.CTkLabel(createEntry, text=categoryText[var], height=12, width=12, text_color="white")
     createEntry.category.grid(row=0, column=1, padx=20, pady=20, sticky="nesw")
 
-    createEntry.categoryEntry = ctk.CTkComboBox(createEntry, values=[categoryGeneral, categoryWork, categoryPrivate], state="readonly")
+    createEntry.categoryEntry = ctk.CTkComboBox(createEntry, values=[categoryGeneral[var], categoryWork[var], categoryPrivate[var]], state="readonly")
     createEntry.categoryEntry.grid(row=0, column=2, columnspan=2, padx=20, pady=20, sticky="ne")
 
-    createEntry.lastname = ctk.CTkLabel(createEntry, text=lastnameText, height=12, width=12, text_color="white")
+    createEntry.lastname = ctk.CTkLabel(createEntry, text=lastnameText[var], height=12, width=12, text_color="white")
     createEntry.lastname.grid(row=1, column=1, padx=20, pady=20, sticky="nesw")
 
     createEntry.lastnameEntry = ctk.CTkEntry(createEntry)
     createEntry.lastnameEntry.grid(row=1, column=2, columnspan=2, padx=20, pady=20, sticky="ne")
 
-    createEntry.firstname = ctk.CTkLabel(createEntry, text=firstnameText, height=12, width=12, text_color="white")
+    createEntry.firstname = ctk.CTkLabel(createEntry, text=firstnameText[var], height=12, width=12, text_color="white")
     createEntry.firstname.grid(row=2, column=1, padx=20, pady=20, sticky="nesw")
 
     createEntry.firstnameEntry = ctk.CTkEntry(createEntry)
@@ -48,13 +60,13 @@ def open_createEntry():
     createEntry.emailEntry = ctk.CTkEntry(createEntry)
     createEntry.emailEntry.grid(row=3, column=2, columnspan=2, padx=20, pady=20, sticky="ne")
 
-    createEntry.phone = ctk.CTkLabel(createEntry, text=phoneText, height=12, width=12, text_color="white")
+    createEntry.phone = ctk.CTkLabel(createEntry, text=phoneText[var], height=12, width=12, text_color="white")
     createEntry.phone.grid(row=4, column=1, padx=20, pady=20, sticky="nesw")
 
     createEntry.phoneEntry = ctk.CTkEntry(createEntry)
     createEntry.phoneEntry.grid(row=4, column=2, columnspan=2, padx=20, pady=20, sticky="ne")
 
-    createEntry.address = ctk.CTkLabel(createEntry, text=addressText, height=12, width=12, text_color="white")
+    createEntry.address = ctk.CTkLabel(createEntry, text=addressText[var], height=12, width=12, text_color="white")
     createEntry.address.grid(row=5, column=1, padx=20, pady=20, sticky="nesw")
 
     createEntry.addressEntry = ctk.CTkEntry(createEntry)
@@ -62,10 +74,17 @@ def open_createEntry():
 
 
     def save():
-        if not (createEntry.categoryEntry.get() and createEntry.lastnameEntry.get() and createEntry.firstnameEntry.get()):
+        if not ((createEntry.categoryEntry.get() and createEntry.lastnameEntry.get() and createEntry.firstnameEntry.get())):
             messagebox.showerror('Error', errorText + "!", parent=createEntry)
         else:
-            categoryInput = createEntry.categoryEntry.get()
+            category = createEntry.categoryEntry.get()
+            if category == categoryGeneral[1]:
+                categoryInput = categoryGeneral[0]
+            elif category == categoryWork[1]:
+                categoryInput = categoryWork[0]
+            elif category == categoryPrivate[1]:
+                categoryInput = categoryPrivate[0]
+                
             lastnameInput = createEntry.lastnameEntry.get()
             firstnameInput = createEntry.firstnameEntry.get()
             emailInput = createEntry.emailEntry.get()
@@ -82,8 +101,8 @@ def open_createEntry():
             createEntry.destroy()
                 
 
-    createEntry.closeButton = ctk.CTkButton(createEntry, text=closeText, height=12, width=12, text_color="white", command=createEntry.destroy)
+    createEntry.closeButton = ctk.CTkButton(createEntry, text=closeText[var], height=12, width=12, text_color="white", command=createEntry.destroy)
     createEntry.closeButton.grid(row=6, column=2, padx=20, pady=20, sticky="ne")
 
-    createEntry.saveButton = ctk.CTkButton(createEntry, text=saveText, height=12, width=12, text_color="white", command=save)
+    createEntry.saveButton = ctk.CTkButton(createEntry, text=saveText[var], height=12, width=12, text_color="white", command=save)
     createEntry.saveButton.grid(row=6, column=3, padx=20, pady=20, sticky="ne")
--- main.py
+++ main.py
@@ -1,19 +1,31 @@
-import tkinter as tk
+from tkinter import messagebox
 import customtkinter as ctk
 from createEntry import *
 from variables import *
 import sqlite3
+from PIL import Image
 
 # ctk.set_default_color_theme("light-pink.json")
 # ctk.set_appearance_mode("light")
 
-language = "english"
-
 connection = sqlite3.connect("addressBook.db")
 cursor = connection.cursor()
 
-createTable = """CREATE TABLE IF NOT EXISTS contacts (category TEXT,lastname TEXT,firstname TEXT,email TEXT,phone INTEGER,address TEXT,date TEXT);"""
+createTable = """CREATE TABLE IF NOT EXISTS contacts (id INTEGER PRIMARY KEY, category TEXT,lastname TEXT,firstname TEXT,email TEXT,phone INTEGER,address TEXT,date TEXT);"""
 cursor.execute(createTable)
+
+getLang = """SELECT language FROM settings;"""
+cursor.execute(getLang)
+language = cursor.fetchone()
+
+if language == None:
+    cursor.execute("INSERT INTO settings (language) VALUES (?)", ("english",))
+    connection.commit()
+
+if language[0] == "german":
+    var = 1
+else:
+    var = 0
 
 
 class root(ctk.CTk):
@@ -35,6 +47,8 @@ class root(ctk.CTk):
 class overview(ctk.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs, fg_color="black", width=500, height=500)
+        for widgets in self.winfo_children():
+            widgets.destroy()
 
         self.grid_rowconfigure(1, weight=0)
         self.grid_rowconfigure(2, weight=1)
@@ -46,15 +60,11 @@ class overview(ctk.CTkFrame):
         self.newEntry = ctk.CTkButton(self, text="+", height=12, width=12, command=open_createEntry)
         self.newEntry.grid(row=1, column=1, padx=(20,10), pady=20, sticky="ne")
 
-        self.settings = ctk.CTkButton(self, text="Settings", height=12, width=12, command=open_settings)
+        self.settings = ctk.CTkButton(self, text=settingsText[var], height=12, width=12, command=open_settings)
         self.settings.grid(row=1, column=2, padx=(10,20), pady=20, sticky="ne")
 
-        self.test = ctk.CTkLabel(self, text=testText, text_color="white")
+        self.test = ctk.CTkLabel(self, text=testText[var], text_color="white")
         self.test.grid(row=1, column=0, padx=20, pady=20)
-
-        # self.sort = ctk.CTkOptionMenu(self, values=[categorySortAll, categoryGeneral, categoryWork, categoryPrivate])
-        # self.sort.grid(row=1, column=0, padx=20, pady=20)
-        # self.sort.set(categorySortAll)
         
         self.table_frame = table(master=self, height=500, width=500)
         self.table_frame.grid(row=3, column=0, columnspan=3, padx=20, pady=20, sticky="nsew")
@@ -65,23 +75,31 @@ class table(ctk.CTkScrollableFrame):
         super().__init__(master, **kwargs)
 
         self.grid_columnconfigure((0,1,2,3,4,5,6), weight=1)
-        self.grid_rowconfigure((0,1,2,3,4,5,6), weight=1)
+        self.grid_columnconfigure((7,8), weight=0)
+        self.grid_rowconfigure((1,2,3,4,5,6), weight=1)
+        self.grid_rowconfigure(0, weight=0)
 
         def getOverview(choice):
-            for widget in self.winfo_children():
-                 if isinstance(widget, ctk.CTkLabel):
-                    widget.destroy()
+            for widgets in self.winfo_children():
+                widgets.destroy()
 
-            if not choice == categorySortAll :
-                overviewSort = """SELECT * FROM contacts WHERE category = ?;"""
-                cursor.execute(overviewSort, (choice,))
-            else:
+            if choice == categorySortAll[0] or choice == categorySortAll[1]:
                 overviewAll = """SELECT * FROM contacts;"""
                 cursor.execute(overviewAll)
+            else:
+                if choice == categoryGeneral[0] or choice == categoryGeneral[1]:
+                    cat = categoryGeneral[0]
+                elif choice == categoryWork[0] or choice == categoryWork[1]:
+                    cat = categoryWork[0]
+                elif choice == categoryPrivate[0] or choice == categoryPrivate[1]:
+                    cat = categoryPrivate[0]
+
+                overviewSort = """SELECT * FROM contacts WHERE category = ?;"""
+                cursor.execute(overviewSort, (cat,))
 
             contacts = cursor.fetchall()
 
-            header = [categoryText, lastnameText, firstnameText, emailText, phoneText, addressText, dateText]
+            header = [categoryText[var], lastnameText[var], firstnameText[var], emailText, phoneText[var], addressText[var], dateText[var]]
 
             c = 0
 
@@ -95,13 +113,23 @@ class table(ctk.CTkScrollableFrame):
             r = 0
 
             for contact in contacts:
-                category = contact[0]
-                lastname = contact[1]
-                firstname = contact[2]
-                email = contact[3]
-                phone = contact[4]
-                address = contact[5]
-                date = contact[6]
+                id = contact[0]
+                category = contact[1]
+                lastname = contact[2]
+                firstname = contact[3]
+                email = contact[4]
+                phone = contact[5]
+                address = contact[6]
+                date = contact[7]
+
+                if var == 1:
+                    if category == categoryGeneral[0]:
+                        category = categoryGeneral[1]
+                    elif category == categoryGeneral[0]:
+                        category = categoryWork[1]
+                    elif category == categoryPrivate[0]:
+                        category = categoryPrivate[1]
+
                 labels[contact] = ctk.CTkLabel(self, text= f"{category}")
                 labels[contact].grid(row=3 + r, column=0, sticky="w")
                 labels[contact] = ctk.CTkLabel(self, text= f"{lastname}")
@@ -117,14 +145,27 @@ class table(ctk.CTkScrollableFrame):
                 labels[contact] = ctk.CTkLabel(self, text= f"{date}")
                 labels[contact].grid(row=3 + r, column=6, sticky="w")
 
+                # edit = ctk.CTkButton(self, text="E", height=12, width=12)
+                # edit.grid(row=3 + r, column=7, padx=10, sticky="w")
+
+                deleteIcon = ctk.CTkImage(light_image=Image.open("icons/delete.png"), dark_image=Image.open("icons/delete.png"), size=(16, 16))
+
+                deleteButton = ctk.CTkButton(self, image=deleteIcon, text="", height=12, width=12, command=lambda: delete(id))
+                deleteButton.grid(row=3 + r, column=8, padx=10, sticky="w")
+
                 r = r + 1
 
 
-        self.sort = ctk.CTkOptionMenu(self, values=[categorySortAll, categoryGeneral, categoryWork, categoryPrivate], command=getOverview)
-        self.sort.grid(row=0, column=6, padx=20, pady=20, sticky="e")
-        self.sort.set(categorySortAll)
+            refreshIcon = ctk.CTkImage(light_image=Image.open("icons/retry.png"), dark_image=Image.open("icons/retry.png"), size=(16, 16))
+
+            self.refresh = ctk.CTkButton(self, image=refreshIcon, text="", command=lambda: getOverview(categorySortAll[0]), height=12, width=12)
+            self.refresh.grid(row=0, column=7, padx=10, pady=20, sticky="e")
+
+            self.sort = ctk.CTkOptionMenu(self, values=[categorySortAll[var], categoryGeneral[var], categoryWork[var], categoryPrivate[var]], command=getOverview, width=40)
+            self.sort.grid(row=0, column=8, padx=10, pady=20, sticky="w")
+            self.sort.set(categorySortAll[var])
         
-        getOverview(categorySortAll)
+        getOverview(categorySortAll[var])
 
 
 class settings(ctk.CTkFrame):
@@ -142,7 +183,7 @@ class settings(ctk.CTkFrame):
         self.theme = ctk.CTkOptionMenu(self, values=["Default", "Pink"], height=12, width=12)
         self.theme.grid(row=3, column=1, padx=20, pady=10, sticky="nesw")
 
-        self.backButton = ctk.CTkButton(self, text="Close", height=12, width=12, command=self.destroy)
+        self.backButton = ctk.CTkButton(self, text=closeText[var], height=12, width=12, command=self.destroy)
         self.backButton.grid(row=4, column=1, padx=20, pady=(10,20))
 
 
@@ -150,33 +191,36 @@ def open_settings():
     root.settings_frame = settings(master=root)
     root.settings_frame.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
 
+def open_restart():
+    restart_win = ctk.CTkToplevel()
+    restart_win.title("Info")
+    restart_win.geometry("200x100")
+    restart_win.attributes('-topmost', 'true')
+    restart_win.focus()
+    restart_win.grab_set() 
+
+    restart_win.info = ctk.CTkLabel(restart_win, text=restartInfo[var], height=12, width=12)
+    restart_win.info.grid(row=0, column=1, padx=20, pady=20, sticky="nesw")
+
+    restart_win.ok = ctk.CTkButton(restart_win, text="Ok", height=12, width=12, command=root.destroy)
+    restart_win.ok.grid(row=1, column=1, padx=20, pady=20, sticky="nesw")
+
 
 def switchLanguage():
-    global language
-    global categoryText
-    global testText
-    global lastnameText
-
-    if language == "german":
-        language = "english"
+    if language[0] == "german":
+        lang = "english"
     else:
-        language = "german"
+        lang = "german"
 
-    if language == "german":
-        categoryText = "Kategorie"
-        testText = "willkommen"
-        lastnameText = "Nachname"
-    elif language == "english":
-        categoryText = "Category"
-        testText = "welcome"
-        lastnameText = "Lastname"
-    
-    update()
+    cursor.execute("UPDATE settings SET language = ?", (lang,))
+    connection.commit()
+
+    open_restart()
 
 
-def update():
-    root.overview_frame.test.configure(text=testText)
-    
+def delete(x):
+    cursor.execute("DELETE FROM contacts WHERE id= ?", (x,))
+    connection.commit()    
 
 root = root()
 root.mainloop()
